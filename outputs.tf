f0e10f613a417e97d35959065a19f4f43f11c6c8
deepak_commercefabric <deepak@fabric.inc>
2020-05-11 15:23:33 +0530

    added o/p

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,27 @@
-# output "id" {
-#   value = azurerm_iothub.iothub.id
-# }
+output "id" {
+  value = azurerm_iothub.iothub.id
+}
+
+output "event_hub_events_endpoint" {
+  value = azurerm_iothub.iothub.event_hub_events_endpoint
+}
+
+output "event_hub_events_path" {
+  value = azurerm_iothub.iothub.event_hub_events_path
+}
+
+output "event_hub_operations_endpoint" {
+  value = azurerm_iothub.iothub.event_hub_operations_endpoint
+}
+
+output "event_hub_operations_path" {
+  value = azurerm_iothub.iothub.event_hub_operations_path
+}
+
+output "hostname" {
+  value = azurerm_iothub.iothub.hostname
+}
+
+output "shared_access_policy" {
+  value = azurerm_iothub.iothub.shared_access_policy
+}
